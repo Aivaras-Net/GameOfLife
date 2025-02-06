5c36e217ce0d542c95d282cbf84f431a2a946dc6
aivaras.armalis <aivaras.net@gmail.com>
2025-02-06 14:21:54 +0200

    feat: rendering preparation for iteration 3

--- src/GameOfLife.Core/Interfaces/IRenderer.cs
+++ src/GameOfLife.Core/Interfaces/IRenderer.cs
@@ -3,13 +3,28 @@
     public interface IRenderer
     {
         /// <summary>
-        /// Renders the current state of the game.
+        /// Renders the game field along with controls and statistics.
         /// </summary>
-        /// <param name="field">A two dimentional boolean array representing the state of cells in a game field.</param>
-        void Render(bool[,] field);
+        /// <param name="field">A two-dimensional boolean array representing the state of cells in the game field.</param>
+        /// <param name="iteration">The current iteration count.</param>
+        /// <param name="livingCells">The number of living cells.</param>
+        /// <param name="offsetX">The left offset for rendering.</param>
+        /// <param name="offsetY">The top offset for rendering.</param>
+        void Render(bool[,] field, int iteration, int livingCells, int offsetX, int offsetY);
 
-        void RenderStatistics(int iteration, int livingCells, int fieldHeight);
+        /// <summary>
+        /// Renders game statistics above the game field.
+        /// </summary>
+        /// <param name="iteration">The current iteration count.</param>
+        /// <param name="livingCells">The number of living cells.</param>
+        /// <param name="offsetX">The left offset for rendering the statistics.</param>
+        /// <param name="offsetY">The top offset for rendering the statistics.</param>
+        void RenderStatistics(int iteration, int livingCells, int offsetX, int offsetY);
 
+        /// <summary>
+        /// Renders a temporary message.
+        /// </summary>
+        /// <param name="message">The message to display.</param>
         void RenderMessage(string message);
     }
 }
